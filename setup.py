1f5c3dab20e8cdf824bfb88d5aaf0a37c151ea5d
SleepyStew <sleepystew3@gmail.com>
2023-06-24 00:21:31 +1000

    Version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="GPTConnect",
-    version="0.1.2",
+    version="0.1.3",
     author="SleepyStew",
     description="A python package to make GPT functions easy",
     packages=["gptconnect"],
