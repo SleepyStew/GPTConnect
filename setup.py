10af30364e6139f651e6beb082ba45c373d86909
SleepyStew <sleepystew3@gmail.com>
2023-06-24 00:31:19 +1000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 
 setuptools.setup(
     name="GPTConnect",
-    version="0.1.4",
+    version="0.1.5",
     author="SleepyStew",
     description="A python package to make GPT functions easy",
     packages=["gptconnect"],
     install_requires=["openai"],
-    github_url="https://github.com/SleepyStew/gptconnect",
+    url="https://github.com/SleepyStew/gptconnect",
     long_description=open("README.md").read(),
 )
